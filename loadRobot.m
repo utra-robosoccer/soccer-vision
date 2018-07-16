40a742b51f4a01be6d002b9f8cfd2be6a23d1caf
Jason Wang <vuwij@me.com>
2018-07-15 23:52:59 -0400

    Fixed code generation for ARM devices

--- loadRobot.m
+++ loadRobot.m
@@ -1,12 +1,18 @@
+% Load Paths
+addpath('../soccer-control')
+addpath('../soccer-vision')
+addpath('../soccer-utility')
 addpath('media')
+addpath('vectormath')
+
+% Connect Robot
+connectRobot;
 
 % Line detection parameters
 clusterProximityThreshold = 0.05;
 netAngleThreshold = pi/10;
 scalerho = 1/500;
 lineTrackingThreshold = 0.2; % Cannot jump 1 per time step
-
-addpath('vectormath')
 
 % Test files
 videotestfile = strcat(pwd,'/media/videos/2.mp4');
